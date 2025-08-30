97cac08fc42e7fdea2e88149075ad349ebae2fac
Jack Wright <28375223+jack3898@users.noreply.github.com>
2025-08-30 23:57:09 +0100

    organise code even more

---
+++ src/file.ts
@@ -0,0 +1,46 @@
+import { $ } from "bun";
+import { join, basename, dirname, extname } from "path";
+
+const videos: ReadonlySet<string> = new Set([
+  ".mp4",
+  ".MP4",
+  ".m4v",
+  ".M4V",
+  ".mkv",
+  ".MKV",
+]);
+
+export async function compressInPlace(pathString: string): Promise<void> {
+  if (videos.has(extname(pathString))) {
+    await compressVideo(pathString);
+  }
+}
+
+async function compressVideo(pathString: string): Promise<void> {
+  const dir = dirname(pathString);
+  const name = basename(pathString);
+  const compressedFilePath = join(dir, `TEMP-${name}`);
+
+  await $`HandBrakeCLI -i ${pathString} -o ${compressedFilePath} -e x265 -q 21 --all-audio --all-subtitles --subtitle-burned=none --audio-copy-mask aac,ac3,mp3,dts --audio-fallback ffac3`.quiet();
+  await $`rm ${pathString}`;
+  await $`mv ${compressedFilePath} ${pathString}`;
+}
+
+export async function sha256HashFile(filePath: string): Promise<string> {
+  const file = Bun.file(filePath).stream();
+
+  const hasher = new Bun.CryptoHasher("sha256");
+  const reader = file.getReader();
+
+  while (true) {
+    const { value, done } = await reader.read();
+
+    if (done) {
+      break;
+    }
+
+    hasher.update(value);
+  }
+
+  return hasher.digest("hex");
+}
--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,55 @@
-import path, { basename, dirname } from "path";
+import path, { dirname } from "path";
 import { getArgs } from "./args";
 import { $, Glob } from "bun";
+import { compressInPlace, sha256HashFile } from "./file";
 
 const args = await getArgs();
 
 await $`rm -rf ${args.clone}`;
 await $`mkdir ${args.clone}`;
 
-if (args.clone) {
-  console.log(`Copying to ${args.clone}...`);
-
-  await $`cp -R ${args.source}/* ${args.clone}`;
-}
-
-const source = args.clone ?? args.source;
-
-const sourceFilesIterator = new Glob("**/*").scan(source);
-const videoFilesIterator = new Glob("**/*.[mM][pP]4").scan(source);
+const sourceFiles = new Glob("**/*").scan(args.source);
 
 type ManifestItem = {
   path: string;
   sha256: string;
-  sizeGB: number;
+  sizeMB: number;
 };
 
 const manifest: ManifestItem[] = [];
 
-console.log("Searching for mp4s to compress...");
+console.log("Starting the compression of media...");
 
-for await (const sourceFile of videoFilesIterator) {
-  const sourceFilePath = path.join(source, sourceFile);
-  const dir = dirname(sourceFilePath);
-  const name = basename(sourceFilePath);
-  const compressedFilePath = path.join(dir, `TEMP-${name}`);
+for await (const sourceFile of sourceFiles) {
+  console.log(`Processing ${sourceFile}`);
 
-  console.log(`Compressing ${sourceFilePath}...`);
+  const sourceFilePath = path.join(args.source, sourceFile);
+  const clonedFilePath = path.join(args.clone, sourceFile);
 
-  await $`HandBrakeCLI -i ${sourceFilePath} -o ${compressedFilePath} -e x265 -q 21 --all-audio --all-subtitles --subtitle-burned=none --audio-copy-mask aac,ac3,mp3,dts --audio-fallback ffac3`.quiet();
-  await $`rm ${sourceFilePath}`;
-  await $`mv ${compressedFilePath} ${sourceFilePath}`;
+  await $`mkdir -p ${dirname(clonedFilePath)}`;
+  await $`cp ${sourceFilePath} ${clonedFilePath}`;
+  await compressInPlace(clonedFilePath);
 }
 
-console.log("Generating manifest file...");
+console.log("Generating a manifest file...");
 
-for await (const sourceFile of sourceFilesIterator) {
-  const sourceFilePath = path.join(args.source, sourceFile);
-  const file = Bun.file(sourceFilePath).stream();
-  const sizeGB = Bun.file(sourceFilePath).size / 1000 ** 3;
-  const hasher = new Bun.CryptoHasher("sha256");
-  const reader = file.getReader();
+const clonedFiles = new Glob("*/**").scan(args.clone);
 
-  while (true) {
-    const { value, done } = await reader.read();
+for await (const clonedFile of clonedFiles) {
+  const clonedFilePath = path.join(args.clone, clonedFile);
 
-    if (done) {
-      break;
-    }
+  console.log(`Calculating meta for ${clonedFilePath}`);
 
-    hasher.update(value); // value is a Uint8Array chunk
-  }
-
-  const hex = hasher.digest("hex");
+  const sha256 = await sha256HashFile(clonedFilePath);
+  const sizeMB = Bun.file(clonedFilePath).size / 1000 ** 2;
 
   manifest.push({
-    path: sourceFile,
-    sha256: hex,
-    sizeGB,
+    path: clonedFile,
+    sha256,
+    sizeMB,
   });
 }
 
-Bun.file(path.join(source, "manifest.json")).write(
+Bun.file(path.join(args.clone, "manifest.json")).write(
   JSON.stringify(manifest, null, 2)
 );
